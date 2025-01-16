8e8ab93f8f65306e413e049fc9525d922071156e
Dezső Vince <2005bnm@gmail.com>
2025-01-16 10:35:22 +0100

    commentek kitörlése

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ for i in range(0,51,1):
     szam1 = random.randint(1,100)
     szamok.append(szam1)
 
-#szin.szin()
-#hetes = oszthato.hettelOszthato(szamok)
-#print(f"A listában {hetes} darab héttel osztható szám van.")
+szin.szin()
+hetes = oszthato.hettelOszthato(szamok)
+print(f"A listában {hetes} darab héttel osztható szám van.")
 sorok = autom.fajl()
 melyik = int(input("Sorban hanyadik auto: "))
 melyik -= 1
